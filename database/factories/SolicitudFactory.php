1df91d2b2b19da41e738a7e512e913a907805b7a
Paoran Abascal <paoran_0705@hotmail.com>
2020-01-07 14:45:16 -0600

    se agregan cometarios a Factory Parte y solicitud

--- database/factories/SolicitudFactory.php
+++ database/factories/SolicitudFactory.php
@@ -11,11 +11,16 @@ use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Solicitud::class, function (Faker $faker) {
+  // se llama el factory de abogado y user para crear un registro y probar su relacion
   $abogado = factory(\App\Abogado::class)->create();
+  $usuario = factory(\App\User::class)->create();
+  // al ser catalogos se mada a llamar de forma aleatoria
+  // estatus solicitud, motivo solicitud, centro,
+  //  ya que se segura que existen registros al generar la migracion
   $estatus_solicitud = EstatusSolicitud::inRandomOrder()->first();
   $motivo_solicitud = MotivoSolicitud::inRandomOrder()->first();
   $centro = Centro::inRandomOrder()->first();
-  $usuario = factory(\App\User::class)->create();
+  // se crea el registro de Solicitud usando los datos obtenidos anteriormente
     return [
         'abogado_id' => $abogado->id,
         'estatus_solicitud_id' => $estatus_solicitud->id,
