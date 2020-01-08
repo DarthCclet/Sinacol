c31d39cd04e93c1d2c4fb8d5f148d329db82afa3
eduardo <eduado_sz5k4@hotmail.com>
2020-01-08 03:17:18 -0600

    Se Cambiaron los factotus para agregar datos a las tablas de las relaciones en lugar de consultar

--- database/factories/ComparecienteFactory.php
+++ database/factories/ComparecienteFactory.php
@@ -6,7 +6,11 @@ use App\Compareciente;
 use Faker\Generator as Faker;
 
 $factory->define(Compareciente::class, function (Faker $faker) {
+    $audiencia = factory(App\Audiencia::class)->create();
+    $parte = factory(App\Parte::class)->create();
     return [
-        //
+        'audiencia_id' => $audiencia->id,
+        'parte_id' => $parte->id,
+        'presentado' => $faker->boolean,
     ];
 });
