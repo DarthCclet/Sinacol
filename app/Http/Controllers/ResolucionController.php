303bb711e206bd324ea0932a9f3547391320e44b
eduardo <eduado_sz5k4@hotmail.com>
2020-01-16 00:16:48 -0600

    Controlador de resoluciones

---
+++ app/Http/Controllers/ResolucionController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Resolucion;
+use Illuminate\Http\Request;
+
+class ResolucionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Resolucion::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return 4;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //Instanciamos la clase Resolucion
+        $resolucion = new Resolucion;
+        //Declaramos la resolucion con el dato enviado en el request
+        $resolucion->resolucion = $request->resolucion;
+        //Guardamos el cambio en nuestro modelo
+        $resolucion->save();
+        return response()->json($resolucion, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return Resolucion::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        return 1;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $res = Resolucion::find($id);
+        $res->update($request->all());
+
+        return response()->json($res, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Resolucion::find($id)->delete();
+        return 204;
+    }
+}
