c7721b7cfd4566b9117ae7efe626fc53d60f1326
edgar-orozco <edgar.orozco@bxd.mx>
2020-01-17 00:27:33 -0600

    Se agrega test para relación usuario con persona y relación usuario con solicitudes

--- tests/Feature/UserApiTest.php
+++ tests/Feature/UserApiTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Solicitud;
 use App\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -17,81 +18,68 @@ class UserApiTest extends TestCase
      * @var array
      */
     private $jsonPaginado = [
-        "success",
-        "data" =>
+        'success',
+        'data' =>
             [
-                "current_page",
-                "data" => [
+                'current_page',
+                'data' => [
                     [
-                        "id",
-                        "name",
-                        "email",
-                        "email_verified_at",
-                        "deleted_at",
-                        "created_at",
-                        "updated_at",
-                        "persona_id"
+                        'id',
+                        'name',
+                        'email',
+                        'email_verified_at',
+                        'deleted_at',
+                        'created_at',
+                        'updated_at',
+                        'persona_id'
                     ]
                 ],
-                "first_page_url",
-                "from",
-                "last_page",
-                "last_page_url",
-                "next_page_url",
-                "path",
-                "per_page",
-                "prev_page_url",
-                "to",
-                "total"
+                'first_page_url',
+                'from',
+                'last_page',
+                'last_page_url',
+                'next_page_url',
+                'path',
+                'per_page',
+                'prev_page_url',
+                'to',
+                'total'
             ],
-        "message"
+        'message'
     ];
 
     /**
-     * Estructura esperada cuando se solicita una persona
+     * Estructura esperada de persona
      * @var array
      */
-    private $jsonConPersona = [
-        "success",
-        "data" =>
-            [
-                "current_page",
-                "data" => [
-                    [
-                        "id",
-                        "name",
-                        "email",
-                        "email_verified_at",
-                        "deleted_at",
-                        "created_at",
-                        "updated_at",
-                        "persona_id",
-                        "persona" => [
-                            "id", "nombre", "primer_apellido", "segundo_apellido","razon_social",
-                            "curp", "rfc", "fecha_nacimiento", "tipo_persona_id", "deleted_at", "updated_at",
-                            "created_at"
-                        ],
-                    ]
-                ],
-                "first_page_url",
-                "from",
-                "last_page",
-                "last_page_url",
-                "next_page_url",
-                "path",
-                "per_page",
-                "prev_page_url",
-                "to",
-                "total"
-            ],
-        "message"
+    private $jsonPersona = [
+        'persona' => [
+            'id', 'nombre', 'primer_apellido', 'segundo_apellido', 'razon_social', 'curp',
+            'rfc', 'fecha_nacimiento', 'tipo_persona_id', 'deleted_at', 'updated_at', 'created_at'
+        ]
     ];
 
     /**
+     * Estructura esperada de una solicitud
+     * @var array
+     */
+    private $jsonSolicitudes = [
+        'solicitudes' => [
+            [
+                'id', 'abogado_id', 'estatus_solicitud_id', 'motivo_solicitud_id',
+                'centro_id', 'user_id', 'ratificada', 'fecha_ratificacion', 'fecha_recepcion',
+                'observaciones', 'presenta_abogado', 'deleted_at', 'created_at', 'fecha_conflicto'
+            ]
+        ]
+    ];
+
+    /**
+     * Una consulta default debe regresar los registros paginados
+     *
      * @test
      * @return void
      */
-    public function unaConsultaDefaultDebeRegresarRegistrosPaginados(): void
+    public function registrosPaginados(): void
     {
         factory(User::class, 100)->create();
 
@@ -107,29 +95,53 @@ class UserApiTest extends TestCase
     }
 
     /**
+     * Al solicitar la siguiente página debe avanzar el contador de página en respuesta
+     *
      * @test
      * @return void
      */
-    public function alSolicitarLaSiguientePaginaDebeAvanzarElContadorDePaginaEnRespuesta(): void
+    public function siguientePagina(): void
     {
         factory(User::class, 100)->create();
-        $response = $this->json('GET', '/api/user', ['page' => 2]);
+        $response = $this->json('GET', '/api/user?page=2');
         $response->assertStatus(200);
         $res = $response->json();
         $this->assertEquals(2, $res['data']['current_page']);
     }
 
     /**
+     * Al solicitar la relación persona debe regresar la estructura con persona
+     *
      * @test
      * @return void
      */
-    public function alSolicitarLaRelacionPersonaDebeRegresarLaestructuraConPersona(): void
+    public function usuarioConPersona(): void
     {
-        factory(User::class, 100)->create();
-        $response = $this->json('GET', '/api/user', ['load' => 'persona']);
-        $response->assertStatus(200);
-        //La estructura debe corresponder a lo esperado
-        $response->assertJsonStructure($this->jsonConPersona);
+        $jsonConPersona = $this->jsonPaginado;
+        $jsonConPersona['data']['data'][0]['persona'] = $this->jsonPersona['persona'];
 
+        factory(User::class, 100)->create();
+
+        $response = $this->json('GET', '/api/user?load=persona');
+        $response->assertStatus(200);
+        $response->assertJsonStructure($jsonConPersona);
+    }
+
+    /**
+     * Al solicitar la relación solicitudes debe regresar la estructura con solicitudes
+     *
+     * @test
+     * @return void
+     */
+    public function usuarioConSolicitudes(): void
+    {
+        $jsonConSolicitudes = $this->jsonPaginado;
+        $jsonConSolicitudes['data']['data'][0]['solicitudes'] = $this->jsonSolicitudes['solicitudes'];
+
+        factory(Solicitud::class, 100)->create();
+        $response = $this->json('GET', '/api/user?load=solicitudes');
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure($jsonConSolicitudes);
     }
 }
