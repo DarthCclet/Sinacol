eefa8ba53dedd378df35f76bdb9f95a71d616a73
edgar-orozco <edgar.orozco@bxd.mx>
2020-01-26 23:05:04 -0600

    Se agrega script sweet alert al include de scripts datatables

--- resources/views/includes/component/datatables.blade.php
+++ resources/views/includes/component/datatables.blade.php
@@ -8,4 +8,5 @@
     <script src="/assets/plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
     <script src="/assets/plugins/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
     <script src="/assets/plugins/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+    <script src="/assets/plugins/sweetalert/dist/sweetalert.min.js"></script>
 @endpush
