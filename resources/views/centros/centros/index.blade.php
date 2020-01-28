a3e0fa64a3022c3166a27f0a274da01e7ef92288
eduardo <eduado_sz5k4@hotmail.com>
2020-01-28 14:35:04 -0600

    Se agregaron vistas de desponibilidad de centros

--- resources/views/centros/centros/index.blade.php
+++ resources/views/centros/centros/index.blade.php
@@ -3,16 +3,7 @@
 @section('title', 'Centros de conciliación')
 
 @include('includes.component.datatables')
-@push('css')
-<link href="/assets/plugins/bootstrap-datepicker/dist/css/bootstrap-datepicker.css" rel="stylesheet" />
-	<link href="/assets/plugins/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css" rel="stylesheet" />
-	<link href="/assets/plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css" rel="stylesheet" />
-	<link href="/assets/plugins/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet" />
-	<link href="/assets/plugins/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" rel="stylesheet" />
-
-	<link href="/assets/plugins/switchery/switchery.min.css" rel="stylesheet" />
-	<link href="/assets/plugins/abpetkov-powerange/dist/powerange.min.css" rel="stylesheet" />
-@endpush
+@include('includes.component.pickers')
 
 @section('content')
 
@@ -320,15 +311,4 @@
             return arreglo;
         }
     </script>
-@endpush
-@push('scripts')
-	<script src="/assets/plugins/moment/moment.js"></script>
-	<script src="/assets/plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.js"></script>
-	<script src="/assets/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js"></script>
-	<script src="/assets/plugins/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
-        
-        
-	<script src="/assets/plugins/switchery/switchery.min.js"></script>
-	<script src="/assets/plugins/abpetkov-powerange/dist/powerange.min.js"></script>
-	<script src="/assets/js/demo/form-slider-switcher.demo.js"></script>
 @endpush
