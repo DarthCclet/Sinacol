76b533432fb0f4fd8fc8b0eb1b975f8f10e4b6a2
eduardo <eduado_sz5k4@hotmail.com>
2020-01-28 14:36:56 -0600

    Include de pickers

---
+++ resources/views/includes/component/pickers.blade.php
@@ -0,0 +1,25 @@
+@push('css')
+    <!--Librerias de date pickers-->
+    <link href="/assets/plugins/bootstrap-datepicker/dist/css/bootstrap-datepicker.css" rel="stylesheet" />
+    <link href="/assets/plugins/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css" rel="stylesheet" />
+    <link href="/assets/plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css" rel="stylesheet" />
+    <link href="/assets/plugins/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet" />
+    <link href="/assets/plugins/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" rel="stylesheet" />
+    <!--Librerias de switch-->
+    <link href="/assets/plugins/switchery/switchery.min.css" rel="stylesheet" />
+    <link href="/assets/plugins/abpetkov-powerange/dist/powerange.min.css" rel="stylesheet" />
+@endpush
+
+@push('scripts')
+    <!--Librerias de date pickers-->
+    <script src="/assets/plugins/moment/moment.js"></script>
+    <script src="/assets/plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.js"></script>
+    <script src="/assets/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js"></script>
+    <script src="/assets/plugins/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+        
+        
+    <!--Librerias de switch-->
+    <script src="/assets/plugins/switchery/switchery.min.js"></script>
+    <script src="/assets/plugins/abpetkov-powerange/dist/powerange.min.js"></script>
+    <script src="/assets/js/demo/form-slider-switcher.demo.js"></script>
+@endpush
