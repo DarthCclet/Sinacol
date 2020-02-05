7c2f3819c9538718f46805b0f29498f7f9bc9b46
Yadira Villa <yadira@Yadira-Mac.local>
2020-02-05 16:57:40 -0600

    actualizacion a catalogo resolucion

--- database/factories/ResolucionFactory.php
+++ database/factories/ResolucionFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Resolucion::class, function (Faker $faker) {
     return [
         //
-        'resolucion' => $faker->sentence
+        'nombre' => $faker->sentence
     ];
 });
