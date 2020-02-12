3b908a921421d6402a6c81eedb77b304c96f815f
eduardo <eduado_sz5k4@hotmail.com>
2020-02-12 12:29:07 -0600

    vista inicial del calendario

---
+++ resources/views/includes/component/calendar.blade.php
@@ -0,0 +1,10 @@
+@push('css')
+	<link href="/assets/plugins/fullcalendar/dist/fullcalendar.print.css" rel="stylesheet" media='print' />
+	<link href="/assets/plugins/fullcalendar/dist/fullcalendar.min.css" rel="stylesheet" />
+@endpush
+
+@push('scripts')
+	<script src="/assets/plugins/moment/moment.js"></script>
+	<script src="/assets/plugins/fullcalendar/dist/fullcalendar.min.js"></script>
+@endpush
+
