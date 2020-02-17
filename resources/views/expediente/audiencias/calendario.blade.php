052f81ba71470768352e068b080fad41d44ea177
eduardo <eduado_sz5k4@hotmail.com>
2020-02-17 03:53:07 -0600

    Se agregaron las opciones para dos audiencias

--- resources/views/expediente/audiencias/calendario.blade.php
+++ resources/views/expediente/audiencias/calendario.blade.php
@@ -2,6 +2,8 @@
 
 @section('title', 'Calendar')
 
+@include('includes.component.datatables')
+@include('includes.component.pickers')
 @include('includes.component.calendar')
 
 @section('content')
@@ -35,24 +37,88 @@
                 <div class="alert alert-muted">
                     - Selecciona el conciliador y la sala donde se celebrará la audiencia
                 </div>
-                <div class="col-md-12 row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="centro_id" class="col-sm-6 control-label">Conciliador</label>
-                            <div class="col-sm-10">
-                                <select id="conciliador_id" class="form-control">
-                                    <option value="">-- Selecciona un centro</option>
-                                </select>
+                <div id="divAsignarUno">
+                    <div class="col-md-12 row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="conciliador_id" class="col-sm-6 control-label">Conciliador</label>
+                                <div class="col-sm-10">
+                                    <select id="conciliador_id" class="form-control">
+                                        <option value="">-- Selecciona un centro</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="sala_id" class="col-sm-6 control-label">Sala</label>
+                                <div class="col-sm-10">
+                                    <select id="sala_id" class="form-control">
+                                        <option value="">-- Selecciona un centro</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="centro_id" class="col-sm-6 control-label">Sala</label>
-                            <div class="col-sm-10">
-                                <select id="sala_id" class="form-control">
-                                    <option value="">-- Selecciona un centro</option>
-                                </select>
+                </div>
+                <div id="divAsignarDos">
+                    <div class="col-md-12 row">
+                        <div class="col-md-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">Solicitante</h4>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="conciliador_solicitante_id" class="col-sm-6 control-label">Conciliador</label>
+                                            <div class="col-sm-10">
+                                                <select id="conciliador_solicitante_id" class="form-control">
+                                                    <option value="">-- Selecciona un centro</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="sala_solicitante_id" class="col-sm-6 control-label">Sala</label>
+                                            <div class="col-sm-10">
+                                                <select id="sala_solicitante_id" class="form-control">
+                                                    <option value="">-- Selecciona un centro</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">Solicitado</h4>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="conciliador_solicitado_id" class="col-sm-6 control-label">Conciliador</label>
+                                            <div class="col-sm-10">
+                                                <select id="conciliador_solicitado_id" class="form-control">
+                                                    <option value="">-- Selecciona un centro</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="sala_solicitado_id" class="col-sm-6 control-label">Sala</label>
+                                            <div class="col-sm-10">
+                                                <select id="sala_solicitado_id" class="form-control">
+                                                    <option value="">-- Selecciona un centro</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -67,6 +133,10 @@
         </div>
     </div>
 </div>
+<input type="hidden" id="fecha_audiencia">
+<input type="hidden" id="hora_inicio">
+<input type="hidden" id="hora_fin">
+<input type="hidden" id="tipoAsignacion">
 <!-- Fin Modal de disponibilidad-->
 @endsection
 
@@ -92,66 +162,183 @@
                             if(b.type == "month"){ // si es la vista de mes, abrir la vista de semana
                                 $('#calendar').fullCalendar("gotoDate",start);
                                 $(".fc-agendaWeek-button").click();
+                                $("#fecha_audiencia").val(start);
                             }else{
                                 start=moment(start).format('Y-MM-DD HH:mm:ss');
                                 end=moment(end).format('Y-MM-DD HH:mm:ss');
-                                getConciliadores(start,end);
-//                                var title = prompt('Event Title:');
-//                                var eventData;
-//                                if (title) {
-//                                    eventData = {
-//                                            title: title,
-//                                            start: start,
-//                                            end: end
-//                                    };
-//                                    $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
-//                                }
+                                SolicitarAudiencia(start,end);
                             }
                             $('#calendar').fullCalendar('unselect');
                         }
                     },
                     editable: true,
                     eventLimit: true, // allow "more" link when too many events
-                    events: [{
-                        title:'hola',
-                        start: '2020-02-11T00:00:00',
-                        end: '2020-02-11T23:00:00',
-                    },{
-                        title:'hola2',
-                        start: '2020-02-11T23:00:00',
-                        end: '2020-02-12T00:00:00',
-                    }
-                ],
                 });
             });
+            function SolicitarAudiencia(inicio,fin){
+                swal({
+                    title: '¿Concilian juntos?',
+                    text: 'Al oprimir aceptar se asignará solo un consiliador y una sola sala para solicitante y solicitado',
+                    icon: 'warning',
+                    buttons: {
+                        cancel: {
+                            text: 'Separados',
+                            value: null,
+                            visible: true,
+                            className: 'btn btn-default',
+                            closeModal: true,
+                        },
+                        confirm: {
+                            text: 'Aceptar',
+                            value: true,
+                            visible: true,
+                            className: 'btn btn-warning',
+                            closeModal: true
+                        }
+                    }
+                }).then(function(isConfirm){
+                    if(isConfirm){
+                        CargarModal(1,inicio,fin);
+                    }else{
+                        CargarModal(2,inicio,fin);
+                    }
+                });
+            }
+            function CargarModal(aux,inicio,fin){
+                console.log(inicio);
+                console.log(fin);
+                if(aux == 1){
+                    $("#divAsignarUno").show();
+                    $("#divAsignarDos").hide();
+                    $("#tipoAsignacion").val(1);
+                }else{
+                    $("#divAsignarUno").hide();
+                    $("#divAsignarDos").show();
+                    $("#tipoAsignacion").val(2);
+                }
+                getConciliadores();
+                getSalas();
+                $("#hora_inicio").val(inicio);
+                $("#hora_fin").val(fin);
+                $("#modal-asignar").modal("show");
+            }
             getConciliadores = function(fechaInicio,fechaFin){
-//                $.ajax({
-//                    url:"/api/conciliadoresDisponibles",
-//                    type:"POST",
-//                    data:{
-//                        fechaInicio:fechaInicio,
-//                        fechaFin:fechaFin,
-//                        centro_id:1
-//                    },
-//                    dataType:"json",
-//                    success:function(data){
-//                        console.log(data);
-                        $("#modal-asignar").modal("show");
-//                        if(data != null && data != ""){
-//                            $("#conciliador_id").html("<option value=''>-- Selecciona un centro</option>");
-//                            $.each(data.data.data,function(index,element){
-//                                $("#conciliador_id").append("<option value='"+element.id+"'>"+element.nombre+"</option>");
-//                            });
-//                        }else{
-//                            $("#conciliador_id").html("<option value=''>-- Selecciona un centro</option>");
-//                        }
-//                        $("#conciliador_id").select2();
-//                        return "hola";
-//                    }
-//                });
+                $.ajax({
+                    url:"/api/conciliador/ConciliadoresDisponibles",
+                    type:"POST",
+                    data:{
+                        fechaInicio:fechaInicio,
+                        fechaFin:fechaFin,
+                        centro_id:1
+                    },
+                    dataType:"json",
+                    success:function(data){
+                        console.log(data);
+                        if(data != null && data != ""){
+                            $("#conciliador_id,#conciliador_solicitado_id,#conciliador_solicitante_id").html("<option value=''>-- Selecciona un centro</option>");
+                            $.each(data,function(index,element){
+                                $("#conciliador_id,#conciliador_solicitado_id,#conciliador_solicitante_id").append("<option value='"+element.id+"'>"+element.persona.nombre+" "+element.persona.primer_apellido+" "+element.persona.segundo_apellido+"</option>");
+                            });
+                        }else{
+                            $("#conciliador_id,#conciliador_solicitado_id,#conciliador_solicitante_id").html("<option value=''>-- Selecciona un centro</option>");
+                        }
+                        $("#conciliador_id,#conciliador_solicitado_id,#conciliador_solicitante_id").select2();
+                    }
+                });
             };
             function getSalas(fechaInicio,fechaFin){
-                
+                $.ajax({
+                    url:"/api/sala",
+                    type:"GET",
+                    data:{
+                        fechaInicio:fechaInicio,
+                        fechaFin:fechaFin,
+                        centro_id:1
+                    },
+                    dataType:"json",
+                    success:function(data){
+                        console.log(data);
+                        $("#sala_id,#sala_solicitado_id,#sala_solicitante_id").html("<option value=''>-- Selecciona una sala</option>");
+                        if(data.data.data != null && data.data.data != ""){
+                            $.each(data.data.data,function(index,element){
+                                $("#sala_id,#sala_solicitado_id,#sala_solicitante_id").append("<option value='"+element.id+"'>"+element.sala+"</option>");
+                            });
+                        }
+                        $("#sala_id,#sala_solicitado_id,#sala_solicitante_id").select2();
+                    }
+                });
+            }
+            $("#btnGuardar").on("click",function(){
+                var validacion = validarAsignacion();
+                console.log(validacion);
+                if(!validacion.error){
+                    $.ajax({
+                        url:"/api/audiencia/calendarizar",
+                        type:"POST",
+                        data:{
+                            fecha_audiencia:new Date($("#fecha_audiencia").val()).toISOString(),
+                            hora_inicio:$("#hora_inicio").val(),
+                            hora_fin:$("#hora_fin").val(),
+                            audiencia_id:1,
+                            asignacion:validacion.arrayEnvio
+                        },
+                        dataType:"json",
+                        success:function(data){
+                            console.log(data);
+                            if(data != null && data != ""){
+                                
+                            }else{
+                                swal({
+                                    title: 'Algo salio mal',
+                                    text: 'No se registro la audiencia',
+                                    icon: 'warning'
+                                });
+                            }
+                        }
+                    });
+                }else{
+                    swal({
+                        title: 'Algo salio mal',
+                        text: validacion.msg,
+                        icon: 'warning'
+                    });
+                }
+            });
+            function validarAsignacion(){
+                var error = false;
+                var msg="";
+                var arrayEnvio = new Array();
+                if($("#tipoAsignacion").val() == 1){
+                    if($("#sala_id").val() == ""){
+                        error = true;
+                        msg = "Selecciona una sala";
+                    }
+                    if($("#conciliador_id").val() == ""){
+                        error = true;
+                        msg = "Selecciona un conciliador";
+                    }
+                    if(!error){
+                        arrayEnvio.push({sala:$("#sala_id").val(),conciliador:$("#conciliador_id").val(),resolucion:true});
+                    }
+                }else{
+                    if(($("#sala_solicitante_id").val() == "" || $("#sala_solicitado_id").val() == "") || ($("#sala_solicitante_id").val() == $("#sala_solicitado_id").val())){
+                        error = true;
+                        msg = "Asegurate de seleccionar las salas y que no sea la misma";
+                    }
+                    if(($("#conciliador_solicitante_id").val() == "" || $("#conciliador_solicitado_id").val() == "") || ($("#conciliador_solicitante_id").val() == $("#conciliador_solicitado_id").val())){
+                        error = true;
+                        msg = "Asegurate de seleccionar los conciliadores y que no sean el mismo";
+                    }
+                    if(!error){
+                        arrayEnvio.push({sala:$("#sala_solicitante_id").val(),conciliador:$("#conciliador_solicitante_id").val(),resolucion:true});
+                        arrayEnvio.push({sala:$("#sala_solicitado_id").val(),conciliador:$("#conciliador_solicitado_id").val(),resolucion:false});
+                    }
+                }
+                var array = [];
+                array.error=error;
+                array.msg=msg;
+                array.arrayEnvio=arrayEnvio;
+                return array;
             }
         </script>
 @endpush
