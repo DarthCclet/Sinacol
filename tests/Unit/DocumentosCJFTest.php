12c47965b87183b3d7af4672600c39e2a6add656
Yadira Villa <yadira@Yadira-Mac.local>
2020-02-19 16:09:38 -0600

    Se agrega clase para servicio de envio de documentos a CJF, y unit test para documentosCJF

---
+++ tests/Unit/DocumentosCJFTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Services\ComunicacionCJF;
+use GuzzleHttp\Client;
+
+// use GuzzleHttp\Psr7\Response;
+// use GuzzleHttp\Psr7\Request;
+// use GuzzleHttp\Exception\RequestException;
+
+class DocumentosCJFTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+     //Pruebas con archivo original
+     public function testPost()
+     {
+       $comunicacionCJF = new ComunicacionCJF();
+       $envioDocumento = $comunicacionCJF->crearDocumentoCJF();
+       $response = $comunicacionCJF->enviaDocumentoCJF($envioDocumento);
+
+       $this->assertEquals(200, $response->getStatusCode());
+
+       $contentType = $response->getHeaders()["Content-Type"][0];
+       $this->assertEquals("application/json; charset=utf-8", $contentType);
+
+       $recepcionDocumento = json_decode($response->getBody());
+       // dd($recepcionDocumento);
+     }
+
+
+}
