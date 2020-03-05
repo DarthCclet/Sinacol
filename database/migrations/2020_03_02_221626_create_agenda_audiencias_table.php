f638c6bc78790a96906e6990ecbff2ef48ca3aa8
eduardo <eduado_sz5k4@hotmail.com>
2020-03-05 03:31:38 -0600

    Se agrego a las migraciones la tabla agenda audiencias

--- database/migrations/2020_03_02_221626_create_agenda_audiencias_table.php
+++ database/migrations/2020_03_02_221626_create_agenda_audiencias_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSalasAudienciasTable extends Migration
+class CreateAgendaAudienciasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,23 +13,23 @@ class CreateSalasAudienciasTable extends Migration
      */
     public function up()
     {
-        Schema::create('salas_audiencias', function (Blueprint $table) {
-            // llave primaria
-            $table->bigIncrements('id')->comment('PK de la tabla salas_audiencias'); 
-            // id del expediente al que corresponde la audiencia
-            $table->integer('sala_id')->comment('FK de la tabla salas');
-            $table->foreign('sala_id')->references('id')->on('salas');
-            // id del expediente al que corresponde la audiencia
+        Schema::create('agenda_audiencias', function (Blueprint $table) {
+            // Llave primaria de la tabla
+            $table->bigIncrements('id');
+            // id de la audiencia 
             $table->integer('audiencia_id')->comment('FK de la tabla audiencias');
             $table->foreign('audiencia_id')->references('id')->on('audiencias');
+            // id del conciliador al que corresponde la audiencia
+            $table->integer('conciliador_id')->comment('FK de la tabla conciliador');
+            $table->foreign('conciliador_id')->references('id')->on('conciliadores');
+            // id de la sala donde se celebrará la audiencia 
+            $table->integer('sala_id')->comment('FK de la tabla salas');
+            $table->foreign('sala_id')->references('id')->on('salas');
             //indicador de que atiende al solicitante
             $table->boolean('solicitante')->comment('indicador de que atiende al solicitante');
             $table->softDeletes()->comment('Indica la fecha y hora en que el registro se borra logicamente.');
             $table->timestamps();
         });
-        $tabla_nombre = 'salas_audiencias';
-        $comentario_tabla = 'Tabla donde se asignan las salas a las audiencias.';
-        DB::statement("COMMENT ON TABLE $tabla_nombre IS '$comentario_tabla'");
     }
 
     /**
@@ -39,6 +39,6 @@ class CreateSalasAudienciasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('salas_audiencias');
+        Schema::dropIfExists('agenda_audiencias');
     }
 }
