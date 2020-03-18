152cfe81df2687a624281a6d267f9a4ea286f791
YadiraV <vyadira00@gmail.com>
2020-03-18 12:36:31 -0600

    se actualiza ruta para mostrar PDF

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,8 @@ Route::resource('jornadas','JornadaController');
 Route::resource('ocupaciones','OcupacionController');
 Route::resource('plantilla-documentos','PlantillasDocumentosController');
 Route::resource('contadores','ContadorController');
-Route::get('plantilla-documento/imprimirPDF','PlantillasDocumentosController@imprimirPDF');
+Route::get('plantilla-documento/{id}/imprimirPDF','PlantillasDocumentosController@imprimirPDF')->name('plantilla-documento/imprimirPDF');
+
 
 
 Auth::routes(['register' => false]);
