f5e8a7b3060d8b58970bebac6f74f42ca0b348be
Paoran <paoran_0705@hotmail.com>
2020-04-03 11:41:42 -0600

    Se muestran datos en datatable

--- resources/views/expediente/solicitudes/_list.blade.php
+++ resources/views/expediente/solicitudes/_list.blade.php
@@ -35,49 +35,78 @@
 
     </tbody>
 </table> --}}
-<table id="tabla-detalle" class="table table-striped table-bordered table-td-valign-middle">
+<input type="hidden" id="ruta" value="{!! route("solicitudes.edit",1) !!}">
+<table id="tabla-detalle" style="width:100%;" class="table table-striped table-bordered table-td-valign-middle">
     <thead>
       <tr><th>Id</th><th>Estatus</th><th>Folio</th><th>Anio</th><th>Centro</th><th>user</th><th>ratificada</th><th>excepcion</th><th>Fecha Ratificacion</th><th>Fecha Recepcion</th><th>Observaciones</th><th></th><th></th><th></th><th></th><th></th></tr>
     </thead>
-  </table>
+
+</table>
   @push('scripts')
   <script>
        $(document).ready(function() {
+        var ruta = $("#ruta").val();
        var dt = $('#tabla-detalle').DataTable({
             "deferRender": true,
             "ajax": {
               "type": "GET",
               "async": true,
               "url": '/api/solicitudes',
+              "dataSrc": function(json){
+                 var array = new Array();
+                 this.recordsTotal = json.total;
+                 $.each(json.data,function(key, value){
+                    array.push(Object.values(value)); 
+                 });
+                return array;
+              },
+              "dataFilter": function(data){
+                var json = jQuery.parseJSON( data );
+                json.recordsTotal = json.total;
+                json.recordsFiltered = json.total;
+                return JSON.stringify(json);
+              },
               "data": function (d) {
               }
             },
             "columnDefs": [
-              {"targets": [0], "visible": false},
-            {
-              "targets": -1,
-              "defaultContent": '<button class="btn-warning btn-editar"><i class="fa fa-pencil btn-editar"></i></button>&nbsp;&nbsp;<button class="btn-danger btn-helpkit"><i class="fa fa-medkit btn-helpkit"></i></button>',
-            }],
-            "aoColumns": [
-              null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null
-              ],
-            "processing": true,
+              {"targets": [1], "visible": false},
+              {"targets": [3], "visible": false},
+              {"targets": [4], "visible": false},
+              {"targets": [11], "visible": false},
+              {"targets": [12], "visible": false},
+              {"targets": [13], "visible": false},
+              {
+                "targets": -1,
+                "render": function (data, type, row) {
+                        // console.log(row[0]);
+                        
+                        return '<div style="display: inline-block;"><a href="'+ruta.replace('/1/',"/"+row[0]+"/")+'" class="btn btn-xs btn-info"><i class="fa fa-pencil-alt"></i></a><button class="btn btn-xs btn-warning btn-borrar"><i class="fa fa-trash btn-borrar"></i></button></div>';          
+                    }
+                // "defaultContent": '<div style="display: inline-block;"><a href="{{route("solicitudes.edit",['+row[0]+'])}}" class="btn btn-xs btn-info"><i class="fa fa-pencil-alt"></i></a><button class="btn btn-xs btn-warning btn-borrar"><i class="fa fa-trash btn-borrar"></i></button></div>',
+              }
+            ],
+            "serverSide": true, 
             select: true,
             "ordering": false,
             "searching": false,
-            "scrollX": true,
-            "scrollY": $(window).height() - $('#header').height()-200,
-            "scrollColapse": false,
-            "scroller": {
-              "serverWait": 200,
-              "loadingIndicator": true
-            },
-            "serverSide": true,
             "pageLength": 20,
-            "responsive": true,
+            "lengthChange": false,
+            "scrollX": true,
+            "scrollY": $(window).height() - $('#header').height()-400,
+            "scrollColapse": false,
+            
+            "scroller": {
+                "serverWait": 200,
+                "loadingIndicator": true
+            },
+            "responsive": false,
+            "language": {
+              "url": "assets/javascripts/dataTables.es.json"
+            },
             "stateSaveParams": function (settings, data) {
               //data.search.search = "";
-              console.log(data);
+            //   console.log(data);
             }
           });
        });
