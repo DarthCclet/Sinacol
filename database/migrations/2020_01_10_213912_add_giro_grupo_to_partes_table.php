2f6256ed812143b66b534fa7561bd920679631f8
Paoran <paoran_0705@hotmail.com>
2020-04-22 17:16:19 -0500

    Se elimina giro_comercial_id de tabla de parte y se agrega en datos personales

--- database/migrations/2020_01_10_213912_add_giro_grupo_to_partes_table.php
+++ database/migrations/2020_01_10_213912_add_giro_grupo_to_partes_table.php
@@ -15,8 +15,7 @@ class AddGiroGrupoToPartesTable extends Migration
     {
       Schema::table('partes', function (Blueprint $table) {
           //
-          $table->unsignedBigInteger('giro_comercial_id')->nullable()->comment('FK a catálogo de giros comerciales');
-          $table->foreign('giro_comercial_id')->references('id')->on('giro_comerciales');
+          
           $table->unsignedBigInteger('grupo_prioritario_id')->nullable()->comment('FK a catálogo de gruos prioritarios');
           $table->foreign('grupo_prioritario_id')->references('id')->on('grupos_prioritarios');
       });
@@ -31,7 +30,6 @@ class AddGiroGrupoToPartesTable extends Migration
     {
       Schema::table('partes', function (Blueprint $table) {
           //
-          $table->dropColumn('giro_comercial_id');
           $table->dropColumn('grupo_prioritario_id');
       });
     }
