167c9ec13c125f370666f682c7e991429bdcac62
eduardo <eduado_sz5k4@hotmail.com>
2020-04-25 00:57:32 -0500

    Se elimino la funcion de validar rangos de tiempo para usar la del trait

--- app/Http/Controllers/AudienciaController.php
+++ app/Http/Controllers/AudienciaController.php
@@ -12,11 +12,11 @@ use App\Centro;
 use Validator;
 use App\Filters\CatalogoFilter;
 use Illuminate\Support\Facades\DB;
-use DateTime;
-use DateTimeZone;
+use App\Traits\ValidateRange;
 
 class AudienciaController extends Controller
 {
+    use ValidateRange;
     protected $request;
 
     public function __construct(Request $request)
@@ -435,31 +435,5 @@ class AudienciaController extends Controller
             $documento->tipo = pathinfo($documento->ruta)['extension'];
         }
         return $documentos;
-    }
-    /**
-     * Funcion para validar los rangos de tiempo
-     * @param time $start_time1
-     * @param time $end_time1
-     * @param time $start_time2
-     * @param time $end_time2
-     * @return bool
-     * @throws Exception
-     */
-    function rangesNotOverlapOpen($start_time1,$end_time1,$start_time2,$end_time2)
-    {
-      $utc = new DateTimeZone('UTC');
-
-      $start1 = new DateTime($start_time1,$utc);
-      $end1 = new DateTime($end_time1,$utc);
-      if($end1 < $start1)
-        throw new Exception('Range is negative.');
-
-      $start2 = new DateTime($start_time2,$utc);
-      $end2 = new DateTime($end_time2,$utc);
-      if($end2 < $start2)
-        throw new Exception('Range is negative.');
-
-//      return (($start2 >= $end1) || ($end2 <= $start1)) ;
-      return ($end1 <= $start2) || ($end2 <= $start1);
     }
 }
