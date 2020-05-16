6bd5c4fd8105d69840fd29f538e9fead87ca130e
edgar-orozco <edgar.orozco@bxd.mx>
2020-05-16 00:59:39 -0500

    Se agrega eager loading al query de giro_comercial para eliminal el problema de N + 1 en la consulta de giro_comerciales y ambitos.

--- app/Http/Controllers/GiroComercialController.php
+++ app/Http/Controllers/GiroComercialController.php
@@ -26,7 +26,7 @@ class GiroComercialController extends Controller
         $giroComercial = (new CatalogoFilter(GiroComercial::query(), $this->request))
             ->searchWith(GiroComercial::class)
             ->filter();
-        
+
         //Evaluamos si es una consulta de la ruta de catálogos entonces regresamos CSV
         if ($this->request->is('catalogos/*')){
             $archivo_csv = 'CatalogoGirosComerciales.csv';
@@ -35,21 +35,21 @@ class GiroComercialController extends Controller
             $query = $query->withTrashed()->get();
             return $this->sendCSVResponse($query->toArray(),['id','nombre',"codigo","_lft","_rgt","parent_id",'creado','modificado','eliminado'], $archivo_csv);
         }
-        
+
         // Si en el request viene el parametro all entonces regresamos todos los elementos
         // de lo contrario paginamos
         if ($this->request->get('all')) {
-            $giroComercial = $giroComercial->get();
+            $giroComercial = $giroComercial->with('ambito')->get();
         } else {
             $giroComercial->select("id","nombre","codigo","_lft","_rgt","parent_id","created_at as creado","updated_at as modificado","deleted_at as eliminado");
-            $giroComercial = $giroComercial->paginate($this->request->get('per_page', 10));
+            $giroComercial = $giroComercial->with('ambito')->paginate($this->request->get('per_page', 10));
         }
 
         if ($this->request->wantsJson()) {
             return $this->sendResponse($giroComercial, 'SUCCESS');
         }
 
-        $giros = GiroComercial::defaultOrder()->withDepth()->get();
+        $giros = GiroComercial::with('ambito')->defaultOrder()->withDepth()->get();
         return view('admin.giros.index', compact('giros'));
     }
 
@@ -132,7 +132,7 @@ class GiroComercialController extends Controller
             ->searchWith(GiroComercial::class)
             ->filter(false);
         $nombre = $this->request->get('nombre');
-        
+
         // $giros_comerciales = GiroComercial::find(1)->descendants;
         if($nombre != ""){
             $nombre = strtr($nombre,array('a'=> '(a|á)','e'=> '(e|é)','i'=>'(i|í)','o'=> '(o|ó)','u'=> '(u|ú)'));
@@ -140,8 +140,8 @@ class GiroComercialController extends Controller
         }else{
             $giroComercial=$giroComercial->select("id","nombre","codigo","_lft","_rgt","parent_id")->withDepth()->orderBy('codigo','asc')->get();
         }
-        
-        
+
+
         if ($this->request->wantsJson()) {
             return $this->sendResponse($giroComercial, 'SUCCESS');
         }
@@ -163,7 +163,7 @@ class GiroComercialController extends Controller
         $arreglo = $this->CambiarAmbitoParents($giro,$arreglo);
         return $arreglo;
     }
-    
+
     private function CambiarAmbitoParents($giro,$arreglo){
         #validamos si tiene padre
         if($giro->parent_id != "" && $giro->parent_id != null){
@@ -175,7 +175,7 @@ class GiroComercialController extends Controller
             #Si un hijo es diferente al nuevo ambito del giro, automaticamente cambia a mixto
             #Si todas son iguales colocamos el nuevo ambito tambien al padre
             $bandera = true;
-            
+
             #Recorremos los hijos
             foreach($hijos as $hijo){
                 if($hijo->ambito_id != $giro->ambito_id){
@@ -237,12 +237,12 @@ class GiroComercialController extends Controller
         $nivel = $this->request->get('nivel');
         $id = $this->request->get('id');
         $muestraCodigo = $this->request->get('muestraCodigo');
-        
+
         // $giros_comerciales = GiroComercial::find(1)->descendants;
         if($nivel != ""){
             $orden = "nombre";
             if($id != ""){
-                $giroComercial->whereDescendantOf($id);    
+                $giroComercial->whereDescendantOf($id);
             }
             if($muestraCodigo){
                 $orden = "codigo";
@@ -253,12 +253,12 @@ class GiroComercialController extends Controller
         }else{
             $giroComercial=$giroComercial->select("id","nombre","codigo","_lft","_rgt","parent_id")->withDepth()->get();
         }
-        
-        
+
+
         if ($this->request->wantsJson()) {
             return $this->sendResponse($giroComercial, 'SUCCESS');
         }
-        
+
     }
     /**
      * Remove the specified resource from storage.
@@ -279,11 +279,11 @@ class GiroComercialController extends Controller
         }else{
             $giroComercial=$giroComercial->select("id","nombre","codigo","_lft","_rgt","parent_id")->withDepth()->get();
         }
-        
-        
+
+
         if ($this->request->wantsJson()) {
             return $this->sendResponse($giroComercial, 'SUCCESS');
         }
-        
+
     }
 }
