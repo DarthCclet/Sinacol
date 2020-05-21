a32335bdbd7cff795a1d413edf30009c3ef9d5b7
eduardo <eduado_sz5k4@hotmail.com>
2020-05-21 02:01:41 -0500

    Se agrego hasRole al modelo de usuarios

--- app/User.php
+++ app/User.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
@@ -19,7 +20,8 @@ class User extends Authenticatable
         LazyLoads,
         LazyAppends,
         RequestsAppends,
-        AppendPolicies;
+        AppendPolicies,
+        HasRoles;
 
     /**
      * Las relaciones que son cargables.
