e7d13cc0e9a107e3585d6dd5279a0a5e3addbfae
edgar-orozco <edgar.orozco@bxd.mx>
2020-08-28 16:57:11 -0500

    Se corrige título de punto

--- resources/views/asesoria/a10.blade.php
+++ resources/views/asesoria/a10.blade.php
@@ -13,6 +13,7 @@
         <li class="mb-4"><a href="/asesoria/1010" class="text-black h4 mb-4">Me despidieron de manera injusta</a></li>
         <li class="mb-4"><a href="/asesoria/1020" class="text-black h4 mb-4">Renuncié y no me pagaron el finiquito correcto</a></li>
         <li class="mb-4"><a href="/asesoria/1030" class="text-black h4 mb-4">Quiero dejar de trabajar con mi patrón y quiero una compensación</a></li>
+        <li class="mb-4"><a href="/asesoria/1030" class="text-black h4 mb-4">Sigo laborando para mi patrón pero tengo un conflicto con él</a></li>
         <li class="mb-4"><a href="/asesoria/1040" class="text-black h4 mb-4">Quiero reclamar mi derecho de preferencia, antigüedad o ascenso bajo el Capítulo IV de la Ley Federal del Trabajo.</a></li>
     </ol>
 
