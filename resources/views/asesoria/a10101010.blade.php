59fabac662589a7a50ea0e7e49558373917ee74e
edgar-orozco <edgar.orozco@bxd.mx>
2020-08-31 11:51:33 -0500

    Botón de avance en lámina de video en ruta default

--- resources/views/asesoria/a10101010.blade.php
+++ resources/views/asesoria/a10101010.blade.php
@@ -5,6 +5,7 @@
     <div class="embed-responsive embed-responsive-21by9">
         <iframe class="embed-responsive-item" id="vid" src="/assets/img/asesoria/Asesoria-1080p-200828.mp4?autoplay=1"></iframe>
     </div>
+    <a href="/asesoria/1010101010/?origen={{$origen}}" class="btn btn-primary btn-lg m-10 float-right" type="button">Siguiente</a>
     @elseif($origen == '10201010')
         <h2>Video: Prestaciones laborales</h2>
         <a href="/asesoria/1010101010/?origen={{$origen}}" class="btn btn-primary btn-lg m-10 float-right" type="button">Siguiente</a>
