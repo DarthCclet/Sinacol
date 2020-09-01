5488c46b7eb15f68a3170ade13f2be37bf379ac8
edgar-orozco <edgar.orozco@bxd.mx>
2020-09-01 15:25:35 -0500

    Cambio de etiqueta en el botón

--- resources/views/asesoria/a101010101010.blade.php
+++ resources/views/asesoria/a101010101010.blade.php
@@ -5,6 +5,6 @@
     <a href="/asesoria/{{$accion}}?from={{$paso_next}}&source={{$accion}}" class="text-black h4 mb-4">
         <img width="1024" class="mx-auto d-block" src="/assets/img/asesoria/101010101010/{{$asset_paso}}" alt="">
     </a>
-    <a href="/solicitudes/create-public" class="btn btn-primary btn-lg m-10 float-right" type="button">Llenar solicitud de conciliación</a>
+    <a href="/solicitudes/create-public" class="btn btn-primary btn-lg m-10 float-right" type="button">Ir a solicitud de conciliación</a>
 
 @endsection
