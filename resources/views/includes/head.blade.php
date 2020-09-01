aaedb771fc0e4d8a1353391fb93f7ffd76b9f263
edgar-orozco <edgar.orozco@bxd.mx>
2020-09-01 18:05:42 -0500

    Modificación de colores del wizzard

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -9,3 +9,35 @@
 <!-- ================== END BASE CSS STYLE ================== -->
 
 @stack('css')
+<style>
+    .sw-main.sw-theme-default .step-anchor > li.active > a small {
+        color: #B38E5D;
+    }
+    .sw-theme-default > ul.step-anchor > li.clickable > a:hover {
+        color: white !important;
+        background: transparent !important;
+        cursor: pointer;
+    }
+    .sw-theme-default > ul.step-anchor > li > a::after {
+        content: "";
+        background: #B38E5D;
+        height: 2px;
+        position: absolute;
+        width: 100%;
+        left: 0px;
+        bottom: 0px;
+        transition: all 250ms ease 0s;
+        transform: scale(0);
+    }
+    .sw-theme-default > ul.step-anchor > li.clickable > a:hover {
+        color: black !important;
+        background: transparent !important;
+        cursor: pointer;
+    }
+    .sw-theme-default > ul.step-anchor > li.active > a:hover {
+        color: white !important;
+        background: transparent !important;
+        cursor: pointer;
+    }
+
+</style>
