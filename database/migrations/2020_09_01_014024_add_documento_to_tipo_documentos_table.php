c720a1c25fb0a960c18dc3dd9849a80f691528ca
YadiraV <vyadira00@gmail.com>
2020-08-31 22:08:16 -0500

    actualizacion tipo_documentos

---
+++ database/migrations/2020_09_01_014024_add_documento_to_tipo_documentos_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\TipoDocumento;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddDocumentoToTipoDocumentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tipo_documentos', function (Blueprint $table) {
+            $path = base_path('database/datafiles');
+            $json = json_decode(file_get_contents($path . "/tipo_documentos.json"));
+            //Se llena el catalogo desde el arvhivo json tipo_documentos.json
+            foreach ($json->datos as $objeto){
+                $tipoDoc = TipoDocumento::find($objeto->id);
+                if($tipoDoc != null){
+                    $tipoDoc->update(['nombre'=>$objeto->nombre,
+                                    'objetos' => $objeto->objetos
+                    ]);
+                }else{
+                    DB::table('tipo_documentos')->insert(
+                        [
+                            'nombre' => $objeto->nombre,
+                            'tipo_archivo_id' => $objeto->objetos
+                        ]
+                    );
+                }
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tipo_documentos', function (Blueprint $table) {
+            //
+        });
+    }
+}
