1f304db9d57914f33df826c6bc16e905f4a0ac97
edgar-orozco <edgar.orozco@bxd.mx>
2020-09-02 01:55:47 -0500

    Se oculta el breadcrumb al público en general. Sólo se muestra a los autenticados

--- resources/views/expediente/solicitudes/create.blade.php
+++ resources/views/expediente/solicitudes/create.blade.php
@@ -7,27 +7,30 @@
 
 
 @section('content')
-<!-- begin breadcrumb -->
-<ol class="breadcrumb float-xl-right">
-    <li class="breadcrumb-item"><a href="">Home</a></li>
-    <li class="breadcrumb-item"><a href="{!! route("solicitudes.index") !!}">Solicitudes</a></li>
-    <li class="breadcrumb-item"><a href="javascript:;">Crear Solicitud</a></li>
-  </ol>
-  <!-- end breadcrumb -->
+    <!-- begin breadcrumb -->
+    @if(auth()->check())
 
-<div class="panel panel-inverse">
-    <div class="panel-body">
-        {{-- <form action="{{url('solicitudes')}}" method="POST"> --}}
+        <ol class="breadcrumb float-xl-right">
+            <li class="breadcrumb-item"><a href="">Home</a></li>
+            <li class="breadcrumb-item"><a href="{!! route("solicitudes.index") !!}">Solicitudes</a></li>
+            <li class="breadcrumb-item"><a href="javascript:;">Crear Solicitud</a></li>
+        </ol>
+    @endif
+    <!-- end breadcrumb -->
+
+    <div class="panel panel-inverse">
+        <div class="panel-body">
+            {{-- <form action="{{url('solicitudes')}}" method="POST"> --}}
             {{ csrf_field() }}
             @include('expediente.solicitudes._form')
-             
 
-        {{-- </form> --}}
+
+            {{-- </form> --}}
+        </div>
     </div>
-</div>
 @endsection
 @push('scripts')
-<script>
-  var edit = false;
-</script>
+    <script>
+        var edit = false;
+    </script>
 @endpush
