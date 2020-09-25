b831f96d870543947c3ea6ed947c3519e9e6b911
edgar-orozco <edgar.orozco@bxd.mx>
2020-09-24 23:27:47 -0500

    Modificado: Se habilita función para debug de plantillas se pasa sólo el id de la solicitud y los demás datos los llena

--- app/Http/Controllers/HeaderFooterTemplatesController.php
+++ app/Http/Controllers/HeaderFooterTemplatesController.php
@@ -2,21 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Solicitud;
 use App\Traits\GenerateDocument;
+use Illuminate\Http\Request;
+
 
 class HeaderFooterTemplatesController extends Controller
 {
     use GenerateDocument;
 
 
-    public function debug()
+    public function debug(Request $request)
     {
-        $idAudiencia = 12;
-        $idSolicitud = 12;
-        $plantilla_id = 2;
-        $idSolicitante = 23;
-        $idSolicitado = 24;
-        $idConciliador = null;
+        $idSolicitud = $request->get('solicitud_id',1);
+
+        $idAudiencia = $request->get('audiencia_id');
+        $plantilla_id = $request->get('plantilla_id', 1);
+        $idSolicitante = $request->get('solicitante_id');
+        $idSolicitado = $request->get('solicitado_id');
+        $idConciliador = $request->get('conciliador_id');
+        $pdf = $request->exists('pdf');
+
+        $solicitud = Solicitud::find($idSolicitud);
+        if ($solicitud) {
+            if (!$idAudiencia && isset($solicitud->expediente->audiencia->first()->id)) {
+                $idAudiencia = $solicitud->expediente->audiencia->first()->id;
+                if(!$idConciliador){
+                    $idConciliador = $solicitud->expediente->audiencia->first()->conciliador->id;
+                }
+            }
+            if(!$idSolicitante){
+                $idSolicitante =$solicitud->solicitantes->first()->id;
+            }
+            if(!$idSolicitado){
+                $idSolicitado =$solicitud->solicitados->first()->id;
+            }
+        }
 
         $html = $this->renderDocumento(
             $idAudiencia,
@@ -27,7 +48,12 @@ class HeaderFooterTemplatesController extends Controller
             $idConciliador
         );
 
-        return $this->renderPDF($html, $plantilla_id);
+        if($pdf) {
+            return $this->renderPDF($html, $plantilla_id);
+        }
+        else{
+            echo $html; exit;
+        }
     }
 
 }
