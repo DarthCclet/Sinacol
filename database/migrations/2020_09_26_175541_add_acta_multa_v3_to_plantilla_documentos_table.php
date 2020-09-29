ec5eb013451f6061861d7796436907d792ee82d4
YadiraV <vyadira00@gmail.com>
2020-09-29 15:44:43 -0500

    correcion de plantilla acta de multa

--- database/migrations/2020_09_26_175541_add_acta_multa_v3_to_plantilla_documentos_table.php
+++ database/migrations/2020_09_26_175541_add_acta_multa_v3_to_plantilla_documentos_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class AddActaMultaV2ToPlantillaDocumentosTable extends Migration
+class AddActaMultaV3ToPlantillaDocumentosTable extends Migration
 {
     /**
      * Run the migrations.
