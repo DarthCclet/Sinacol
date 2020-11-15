0c73ced7c5a2edf9415d329615e81eb72fd4132f
edgar-orozco <edgar.orozco@bxd.mx>
2020-11-15 02:48:36 -0600

    Agregado: Se agrega imagen mapa del plan de implementación

---
+++ resources/views/asesoria/mapa-implementacion.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('content')
+    <a href="/asesoria/seleccion" class="text-black h4 mb-4">
+        <img width="1024" class="mx-auto d-block" src="/assets/img/asesoria/mapa-plan-implementacion.jpg" alt="">
+    </a>
+@endsection
