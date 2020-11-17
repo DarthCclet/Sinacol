9aabb4412f1cccafcc2073a58c3a5877753c6065
YadiraV <vyadira00@gmail.com>
2020-11-16 23:16:10 -0600

    generar constancia de cumplimiento cuando se hayan pagado todos

--- app/Http/Controllers/AudienciaController.php
+++ app/Http/Controllers/AudienciaController.php
@@ -1279,7 +1279,8 @@ class AudienciaController extends Controller {
             }
             //si los pagos anteriores han sido pagados y es el ultimo pago
             //generar constancia de cumplimiento de convenio
-            if($pagados && ($ultimoPago == $request->idPagoDiferido)){
+            //if($pagados && ($ultimoPago == $request->idPagoDiferido)){
+            if($pagados){
                 event(new GenerateDocumentResolution($request->audiencia_id, $request->solicitud_id, 45, 12));
             }
             return $pagoDiferido;
