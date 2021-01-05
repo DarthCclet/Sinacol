d70f047848508f25540b6e177db0a72f26c4079f
Paoran <paoran_0705@hotmail.com>
2021-01-05 15:46:28 -0600

    Se corrigen estilos de loader en ajax para cubrir pantalla completa

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -49,11 +49,15 @@
     }
     .loading {
         z-index: 9999 !important;
-        position: absolute;
+        position: fixed;
         top: 0;
         left:-5px;
         width: 100%;
         height: 100%;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
         background-color: rgba(0,0,0,0.4);
     }
     .progress-content {
