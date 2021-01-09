0cb0a0394418821271b94488a5aebef727e0227e
gis_a <gis_a@DESKTOP-CV8CQIO>
2021-01-08 18:15:02 -0600

    Se agrega historial de notificaciones

--- resources/views/centros/centros/notificaciones.blade.php
+++ resources/views/centros/centros/notificaciones.blade.php
@@ -68,23 +68,111 @@
                         @endif
                         <td>{{$solicitud['parte']->fecha_notificacion ?? 'pendiente'}}</td>
                         <td>{{$solicitud['parte']->finalizado ?? 'N/A'}}</td>
+                        <td>
                         @if($solicitud['fecha_peticion_notificacion'] != null)
-                            <td>Enviada
+                            Enviada
                             <button onclick="enviar_notificacion({{$solicitud['id']}})" class="btn btn-xs btn-primary incidencia" title="Reenviar notificación">
                                 <i class="fa fa-share-square"></i>
                             </button>
-                            </td>
                         @else
-                            <td>Pendiente
+                            Pendiente
                             <button onclick="enviar_notificacion({{$solicitud['id']}})" class="btn btn-xs btn-primary incidencia" title="Enviar notificación">
                                 <i class="fa fa-paper-plane"></i>
                             </button>
-                            </td>
                         @endif
+                            <button onclick="obtenerHistorial({{$solicitud['parte']->audiencia_parte_id}})" class="btn btn-xs btn-primary incidencia" title="Historial">
+                                <i class="fa fa-history"></i>
+                            </button>
+                        </td>
                     </tr>
                     @endforeach
                 </tbody>
             </table>
+        </div>
+    </div>
+    <div class="modal" id="modal-historico" aria-hidden="true" style="display:none;">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Historial de notificación</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                </div>
+                <div class="modal-body">
+                    <div id="divHistoricoCitatorio">
+                        <h4>Citatorio</h4>
+                        <hr class="red">
+                        <div class="col-md-12 row">
+                            <div class="col-md-6">
+                                <h5>Envios</h5>
+                                <table class="table table-striped table-bordered table-hover" id="tableHistoricoCitatorioEnvios">
+                                    <thead>
+                                        <tr>
+                                            <td>Fecha y hora</td>
+                                            <td>Evento origen</td>
+                                            <td>Fecha de petición</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div id="divHistoricoCitatorioRespuestas" class="col-md-6">
+                                <h5>Respuestas</h5>
+                                <table class="table table-striped table-bordered table-hover" id="tableHistoricoCitatorioRespuestas">
+                                    <thead>
+                                        <tr>
+                                            <td>Fecha y hora</td>
+                                            <td>Respuesta</td>
+                                            <td>Documento</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="divHistoricoMulta">
+                        <h4>Multa</h4>
+                        <hr class="red">
+                        <div class="col-md-12 row">
+                            <div class="col-md-6">
+                                <h5>Envios</h5>
+                                <table class="table table-striped table-bordered table-hover" id="tableHistoricoMultaEnvios">
+                                    <thead>
+                                        <tr>
+                                            <td>Fecha y hora</td>
+                                            <td>Evento origen</td>
+                                            <td>Fecha de petición</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div id="divHistoricoCitatorioRespuestas" class="col-md-6">
+                                <h5>Respuestas</h5>
+                                <table class="table table-striped table-bordered table-hover" id="tableHistoricoMultaRespuestas">
+                                    <thead>
+                                        <tr>
+                                            <td>Fecha y hora</td>
+                                            <td>Respuesta</td>
+                                            <td>Documento</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <div class="text-right">
+                        <a class="btn btn-white btn-sm" data-dismiss="modal"><i class="fa fa-times"></i> Cerrar</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -149,6 +237,93 @@
                             }
                         }
                     });
+                }
+            });
+        }
+        function obtenerHistorial(audiencia_parte_id){
+            $.ajax({
+                url:"/obtenerHistorial/"+audiencia_parte_id,
+                type:"GET",
+                dataType:"json",
+                async:true,
+                success:function(data){
+                    try{
+                        if(data != null){
+                            $("#divHistoricoCitatorio").hide();
+                            $("#divHistoricoMulta").hide();
+                            var div1="";
+                            var div2="";
+                            var div3="";
+                            var div4="";
+                            $.each(data,function(index,element){
+                                $.each(element.respuestas,function(index2,element2){
+                                    if(element.tipo_notificacion == "citatorio"){
+                                        if(element2.etapa_notificacion != null){
+                                            div1 +="<tr>";
+                                            div1 +="<td>"+element2.created_at+"</td>";
+                                            div1 +=' <td>'+element2.etapa_notificacion.etapa+'</td>';
+                                            div1 +=' <td>'+element2.fecha_peticion+'</td>';
+                                            div1 +="</tr>";
+                                        }else{
+                                            div2 +="<tr>";
+                                            div2 +="<td>"+element2.created_at+"</td>";
+                                            div2 +=' <td>'+element2.finalizado+'</td>';
+                                            div2 +=' <td>';
+                                            div2 +='     <a href="/api/documentos/getFile/'+element2.documento.uuid+'" target="_blank" class="btn btn-xs btn-primary incidencia" title="Documento">';
+                                            div2 +='         <i class="fa fa-file-pdf"></i>';
+                                            div2 +='     </a>';
+                                            div2 +=' </td>';
+                                            div2 +="</tr>";
+                                            
+                                        }
+                                    }else{
+                                        if(element2.etapa_notificacion != null){
+                                            div3 +="<tr>";
+                                            div3 +="<td>"+element2.created_at+"</td>";
+                                            div3 +=' <td>'+element2.etapa_notificacion.etapa+'</td>';
+                                            div3 +=' <td>'+element2.fecha_peticion+'</td>';
+                                            div3 +="</tr>";
+                                        }else{
+                                            div4 +="<tr>";
+                                            div4 +="<td>"+element2.created_at+"</td>";
+                                            div4 +=' <td>'+element2.finalizado+'</td>';
+                                            div4 +=' <td>';
+                                            div4 +='     <a href="/api/documentos/getFile/'+element2.documento.uuid+'" target="_blank" class="btn btn-xs btn-primary incidencia" title="Documento">';
+                                            div4 +='         <i class="fa fa-file-pdf"></i>';
+                                            div4 +='     </a>';
+                                            div4 +=' </td>';
+                                            div4 +="</tr>";
+                                        }
+                                    }
+                                });
+                            });
+                            $("#tableHistoricoCitatorioEnvios tbody").html(div1);
+                            $("#tableHistoricoCitatorioRespuestas tbody").html(div2);
+                            $("#tableHistoricoMultaEnvios tbody").html(div3);
+                            $("#tableHistoricoMultaRespuestas tbody").html(div4);
+                            if(div1 != "" || div2 != ""){
+                                $("#divHistoricoCitatorio").show();
+                            }
+                            if(div3 != "" || div4 != ""){
+                                $("#divHistoricoMulta").show();
+                            }
+                            $("#modal-historico").modal("show");
+                        }else{
+                            swal({
+                                title: 'Aviso',
+                                text: 'No hay información de esta notificación',
+                                icon: 'info'
+                            });
+                        }
+                    }catch(error){
+                    console.log(error);
+                        swal({
+                            title: 'Error',
+                            text: 'Algo salio mal con la solicitud',
+                            icon: 'success'
+                        });
+
+                    }
                 }
             });
         }
