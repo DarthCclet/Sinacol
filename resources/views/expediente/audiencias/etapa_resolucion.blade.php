4ae6311e9d4eaf355492eb005bc442d94b69dbef
Paoran <paoran_0705@hotmail.com>
2021-01-26 11:21:53 -0600

    Se permite atender audiencias presenciales para centros que atienden virtual

--- resources/views/expediente/audiencias/etapa_resolucion.blade.php
+++ resources/views/expediente/audiencias/etapa_resolucion.blade.php
@@ -50,6 +50,7 @@
 <h1 class="badge badge-secondary col-md-2 offset-10" style="position: fixed; font-size: 2rem; z-index:999;" onclick="startTimer();"><span class="countdown">00:00:00</span></h1>
 <input type="hidden" id="audiencia_id" name="audiencia_id" value="{{$audiencia->id}}" />
 <input type="hidden" id="atiende_virtual" name="atiende_virtual" value="{{$atiende_virtual}}" />
+<input type="hidden" id="virtual" name="virtual" value="{{$virtual}}" />
 
 @if(auth()->user()->persona_id == $conciliador->persona_id)
 <!-- begin timeline -->
@@ -1452,7 +1453,11 @@
         FormMultipleUpload.init();
         Gallery.init();
         if($("#atiende_virtual").val() == 1){
-            $("#timeline_etapas").hide();
+            if($("#virtual").val() == 1 && $("#url_virtual").val() == ""){
+                $("#timeline_etapas").hide();
+            }else{
+                $("#timeline_etapas").show();
+            }
         }else{
             $("#timeline_etapas").show();
         }
