d7b04c3d47425de4ba0d6115b2a350c0ea72bd1f
Paoran <paoran_0705@hotmail.com>
2021-02-04 19:08:07 -0600

    Se corrige redireccion al terminar orientación en patronal individual

--- resources/views/asesoria/a201010.blade.php
+++ resources/views/asesoria/a201010.blade.php
@@ -3,12 +3,16 @@
 @section('content')
 
     <div class="align-middle">
-        <a href="/asesoria/{{$accion}}?from={{$paso_next}}&source={{$accion}}" class="text-black h4 mb-4">
+        @if($last)
+            <a href="/solicitudes/create-public?solicitud=2" class="text-black h4 mb-4">
+        @else
+            <a href="/asesoria/{{$accion}}?from={{$paso_next}}&source={{$accion}}" class="text-black h4 mb-4">
+        @endif
             <img width="1024" class="mx-auto d-block" src="/assets/img/asesoria/201010/{{$asset_paso}}" alt="">
         </a>
     </div>
     @if($last)
-        <a href="/solicitudes/create-public/?solicitud=2" class="btn btn-primary btn-lg m-10 float-right" type="button">Ir a solicitud de conciliación</a>
+        <a href="/solicitudes/create-public?solicitud=2" class="btn btn-primary btn-lg m-10 float-right" type="button">Ir a solicitud de conciliación</a>
     @endif
 
 @endsection
