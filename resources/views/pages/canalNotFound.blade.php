7afa596a85ce73e5502e87471e699de55ec89d58
Paoran <paoran_0705@hotmail.com>
2021-02-11 18:19:25 -0600

    Se cambia tamaño de letra y margen de texto para canal

--- resources/views/pages/canalNotFound.blade.php
+++ resources/views/pages/canalNotFound.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.empty', ['paceTop' => true])
 <div class="col-md-12 text-center" style="margin: 20% 0 0 0;">
     <i style="font-size: 5rem" class="fa fa-exclamation-circle"></i>
-    <h1 style="font-size: 3rem">{{$mensaje}}</h1>
+    <h1 style="font-size: 2rem; margin:0 10% 0 10%">{{$mensaje}}</h1>
 </div>
