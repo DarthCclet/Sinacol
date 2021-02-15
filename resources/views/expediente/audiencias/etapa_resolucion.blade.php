b76ce12af0968e42f74f251659d6845e7a60ba25
Paoran <paoran_0705@hotmail.com>
2021-02-15 11:33:27 -0600

    Se cambia validacion de guia para que solo valide si es virtual

--- resources/views/expediente/audiencias/etapa_resolucion.blade.php
+++ resources/views/expediente/audiencias/etapa_resolucion.blade.php
@@ -49,12 +49,13 @@
 <!-- end page-header -->
 <h1 class="badge badge-secondary col-md-2 offset-10" style="position: fixed; font-size: 2rem; z-index:999;" onclick="startTimer();"><span class="countdown">00:00:00</span></h1>
 <input type="hidden" id="audiencia_id" name="audiencia_id" value="{{$audiencia->id}}" />
-<input type="hidden" id="atiende_virtual" name="atiende_virtual" value="{{$atiende_virtual}}" />
+{{-- <input type="hidden" id="atiende_virtual" name="atiende_virtual" value="{{$atiende_virtual}}" /> --}}
 <input type="hidden" id="virtual" name="virtual" value="{{$virtual}}" />
 
 @if(auth()->user()->persona_id == $conciliador->persona_id)
 <!-- begin timeline -->
-<div id="confirmacion_virtual" class="col-md-12 row" style="{{($atiende_virtual && $virtual) ? '' : 'display:none'}}">
+{{-- <div id="confirmacion_virtual" class="col-md-12 row" style="{{($atiende_virtual && $virtual) ? '' : 'display:none'}}"> --}}
+<div id="confirmacion_virtual" class="col-md-12 row" style="{{($virtual) ? '' : 'display:none'}}">
     <div class="col-md-2"></div>
     <div class="col-md-8">
         <h5>Proceso virtual</h5>
@@ -64,7 +65,7 @@
         <button class="btn btn-primary" onclick="guardarUrlVirtual()">Guardar</button>
     </div>
 </div>
-<ul class="timeline" id="timeline_etapas" style="display:none">
+<ul class="timeline" id="timeline_etapas" >
     @foreach($etapa_resolucion as $etapa)
         @if($etapa->paso == 1)
             <li style="" id="step{{$etapa->paso}}">
@@ -1459,15 +1460,15 @@
         cargarTipoContactos();
         FormMultipleUpload.init();
         Gallery.init();
-        if($("#atiende_virtual").val() == 1){
-            if($("#virtual").val() == 1 && $("#url_virtual").val() == ""){
+        // if($("#atiende_virtual").val() == 1){
+            if($("#virtual").val() == 1 && $("#url_virtual").val() == "")){
                 $("#timeline_etapas").hide();
             }else{
                 $("#timeline_etapas").show();
             }
-        }else{
-            $("#timeline_etapas").show();
-        }
+        // }else{
+        //     $("#timeline_etapas").show();
+        // }
     });
     $(".dateBirth").datepicker({
         changeMonth: true,
