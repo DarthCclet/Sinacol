17491315bf1f51f72ad539f94595566438171a7b
Paoran <paoran_0705@hotmail.com>
2021-05-11 18:18:25 -0500

    Se comenta la linea de generacion de remosion de citados

--- app/Http/Controllers/SolicitudController.php
+++ app/Http/Controllers/SolicitudController.php
@@ -1097,7 +1097,7 @@ class SolicitudController extends Controller {
                 } else {
                     // Si la variable activo esta en 0 se elimina la parte
                     $parteSaved = Parte::find($citado['id']);
-                    event(new GenerateDocumentResolution(null,$solicitudSaved->id,59,21,null,$parteSaved->id));
+                    //event(new GenerateDocumentResolution(null,$solicitudSaved->id,59,21,null,$parteSaved->id));
                     $parteSaved->delete();
                 }
             }
