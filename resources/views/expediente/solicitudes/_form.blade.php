6f05d641b69d8e1632bd17738b666a8c168914d2
Paoran <paoran_0705@hotmail.com>
2021-05-12 23:04:42 -0500

    Se corrige texto de alerts para formato de fecha

--- resources/views/expediente/solicitudes/_form.blade.php
+++ resources/views/expediente/solicitudes/_form.blade.php
@@ -3035,7 +3035,7 @@
     $(".validaFecha").change(function(){
         var date_regex = /(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$/;
         if(!date_regex.test($(this).val())){
-            swal({title: 'Error',text: ' El formato de la fecha no es correcta ',icon: 'error'});
+            swal({title: 'Error',text: ' El formato de la fecha no es correcta el formato debe ser dd/mm/yyy7 ',icon: 'error'});
             $(this).val("");
         }
     });
