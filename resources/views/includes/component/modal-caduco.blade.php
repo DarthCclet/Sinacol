e00aff587d5eb083565ccb099da42e13f9ce4ad9
Paoran <paoran_0705@hotmail.com>
2021-05-25 19:13:24 -0500

    Se corrige consulta para expirados y se cambia palabra caduca por expira

--- resources/views/includes/component/modal-caduco.blade.php
+++ resources/views/includes/component/modal-caduco.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h3 class="modal-title">Las siguientes <span class="badge badge-pill btn-light">{{count($caducan)}}</span> solicitudes estan por caducar </h3>
+                <h3 class="modal-title">Las siguientes <span class="badge badge-pill btn-light">{{count($caducan)}}</span> solicitudes estan por expirar </h3>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -10,7 +10,7 @@
             <div class="modal-body">
                 <div class="col-md-12" style="overflow: scroll; max-height:400px;">
                     @foreach($caducan as $sol)
-                        <label style="font-size: x-large;">- Solicitud: <b>{{$sol->folio}}/{{$sol->anio}}</b> Caduca en <b>{{$sol->Caduca}}</b> d&iacute;as</label>
+                        <label style="font-size: x-large;">- Solicitud: <b>{{$sol->folio}}/{{$sol->anio}}</b> Expira en <b>{{$sol->Caduca}}</b> d&iacute;as</label>
                     @endforeach
                 </div>
             </div>
