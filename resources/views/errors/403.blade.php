78f50c7859756d99fdccc5ff110cd6c1b93c7548
edgar-orozco <9440039+edgar-orozco@users.noreply.github.com>
2021-05-28 16:17:43 -0500

    Se agrega página de error 403

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.empty', ['paceTop' => true, 'title' => 'PERMISOS INSUFICIENTES'])
+<div class="col-md-12 text-center" style="margin: 20% 0 0 0;">
+    <i style="font-size: 5rem" class="fa fa-exclamation-circle"></i>
+    <h4 style="font-size: 3rem">No cuenta con los permisos suficientes para ejecutar la acción solicitada</h4>
+</div>
