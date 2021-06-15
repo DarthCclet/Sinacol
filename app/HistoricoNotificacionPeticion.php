91cbe21484bf50197b4810c3f72d685d40a4702a
Lalo <eduardo_sz5k4hotmail.com>
2021-06-15 17:38:17 -0500

    Cambio de datos para el historico

---
+++ app/HistoricoNotificacionPeticion.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HistoricoNotificacionPeticion extends Model
+{
+    protected $table = "historico_notificaciones_peticiones";
+    protected $guarded = ['id','created_at','updated_at','deleted_at'];
+
+    public function historico_notificacion(){
+        return $this->belongsTo(HistoricoNotificacion::class,'historico_notificacion_id');
+    }
+    public function historico_notificacion_respuesta(){
+        return $this->hasOne(HistoricoNotificacionRespuesta::class,'historico_notificacion_respuesta_id');
+    }
+}
