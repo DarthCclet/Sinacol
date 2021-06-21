bcf19e162abca24bce52e6624a2d35de008614c6
Paoran <paoran_0705@hotmail.com>
2021-06-21 12:50:18 -0500

    Se agrega migracion para corregir resolucion a audiencias intermedias no ejecutadas

---
+++ database/migrations/2021_06_17_204325_correct_audiencia_resolucion_terminacion.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Audiencia;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CorrectAudienciaResolucionTerminacion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $audiencias = Audiencia::where('tipo_terminacion_audiencia_id',5)->where('resolucion_id',null)->get();
+        foreach ($audiencias as $audiencia) {
+            $audiencia->update(['resolucion_id'=>2]);
+        }
+        $audiencias3 = Audiencia::where('tipo_terminacion_audiencia_id',3)->get();
+        foreach($audiencias3 as $audiencia3){
+            if(!$audiencia3->EsUltima){
+                $audiencia3->update(['resolucion_id'=>2]);
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
