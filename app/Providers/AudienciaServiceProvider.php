7e9db41cbb1a27e876eb3bc12940736be8a723d2
YadiraV <vyadira00@gmail.com>
2021-07-02 15:10:55 -0500

    Se agrega desc ConceptoPago, domicilioLaboral adicional,justificacion pagos,pagos diferidos false,

--- app/Providers/AudienciaServiceProvider.php
+++ app/Providers/AudienciaServiceProvider.php
@@ -58,6 +58,7 @@ class AudienciaServiceProvider extends ServiceProvider
                         ]);
                     }
                 }
+                $totalPagoC = [];
                 if ($bandera) {
                     //Se consulta comparecencia de solicitante
                     $parteS = $solicitante->parte;
@@ -168,6 +169,7 @@ class AudienciaServiceProvider extends ServiceProvider
             if ($solicitanteComparecio != null) {
                 if (isset($listaConceptos)) {
                     if (count($listaConceptos) > 0) {
+                        $totalPagoC[$solicitado->parte_id] = 0.0;
                         foreach ($listaConceptos as $key => $conceptosSolicitante) {//solicitantes
                             if ($key == $solicitado->parte_id) {
                                 foreach ($conceptosSolicitante as $k => $concepto) {
@@ -179,6 +181,7 @@ class AudienciaServiceProvider extends ServiceProvider
                                         "monto" => $concepto["monto"],
                                         "otro" => $concepto["otro"]
                                     ]);
+                                    $totalPagoC[$solicitado->parte_id] =  floatval($totalPagoC[$solicitado->parte_id])  + floatval($concepto["monto"]);
                                 }
                             }
                         }
@@ -206,7 +209,9 @@ class AudienciaServiceProvider extends ServiceProvider
                             "audiencia_id" => $audiencia->id,
                             "solicitante_id" => $fechaPago["idCitado"],
                             "monto" => $fechaPago["monto_pago"],
-                            "fecha_pago" => Carbon::createFromFormat('d/m/Y h:i', $fechaPago["fecha_pago"])->format('Y-m-d h:i')
+                            "descripcion_pago" => $fechaPago["descripcion_pago"],
+                            "fecha_pago" => Carbon::createFromFormat('d/m/Y h:i', $fechaPago["fecha_pago"])->format('Y-m-d h:i'),
+                            "diferido"=>true
                         ]);
                     }
                 }
@@ -222,6 +227,17 @@ class AudienciaServiceProvider extends ServiceProvider
                     }
                     $convenio = ResolucionPartes::where('parte_solicitante_id', $solicitante->parte_id)->where('parte_solicitada_id', $solicitado->parte_id)->where('terminacion_bilateral_id', 3)->first();
                     if ($convenio != null) {
+                        if (!isset($listaFechasPago)) {//si no se registraron pagos diferidos crear pago NO diferido
+                            ResolucionPagoDiferido::create([
+                                "audiencia_id" => $audiencia->id,
+                                "solicitante_id" => $solicitado->parte_id,
+                                "monto" => $totalPagoC[$solicitado->parte_id],
+                                "descripcion_pago" => "Convenio",
+                                "fecha_pago" => $audiencia->fecha_audiencia ." ".$audiencia->hora_fin,
+                                "diferido"=>false
+                            ]);
+                        }
+
                         if($convenio->tipo_propuesta_pago_id == 4){
                             //generar convenio reinstalacion
                             event(new GenerateDocumentResolution($audiencia->id, $audiencia->expediente->solicitud->id, 43, 15, $solicitante->parte_id, $solicitado->parte_id));
