ec960ee30ed5bbfb75914332d0a2f1b79f809f88
Lalo <eduardo_sz5k4hotmail.com>
2021-07-21 18:23:14 -0500

    Corrección de audiencia asignable

--- resources/views/buzon/buzon.blade.php
+++ resources/views/buzon/buzon.blade.php
@@ -63,7 +63,7 @@
                             @if($key == 0)
                                 @if($solicitud->expediente != null)
                                     @if(count($solicitud->expediente->audiencia) > 0)
-                                        @if(!$solicitud->expediente->audiencia()->orderBy("id","desc")->first()->solicitud_cancelacion || !$solicitud->expediente->audiencia()->orderBy("id","desc")->first()->finalizada )
+                                        @if(!$solicitud->expediente->audiencia()->orderBy("id","desc")->first()->solictud_cancelcacion && !$solicitud->expediente->audiencia()->orderBy("id","desc")->first()->finalizada )
                                         <button class="btn btn-primary btn-small pull-right" onclick="reprogramarAudiencia({{$solicitud->expediente->audiencia()->orderBy("id","desc")->first()->id}},'{{$solicitud->acepto_buzon}}')">Reprogramar audiencia </button>
                                         @endif
                                     @endif
