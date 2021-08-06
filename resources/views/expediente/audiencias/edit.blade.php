ef50c2fe740e1a3a5c89cfd957569d8e23972212
Eduardo Sánchez Zarco <eduardo_sz5k4@hotmail.com>
2021-08-06 13:26:52 -0500

    Se agrega variable faltante en generación de nuevas audiencias

--- resources/views/expediente/audiencias/edit.blade.php
+++ resources/views/expediente/audiencias/edit.blade.php
@@ -1445,6 +1445,7 @@
         var finalizada=false;
         var listaResolucionesIndividuales=[];
         var origen = 'audiencias';
+        var origen_vista = 'audiencias';
         var url = document.location.toString();
         if (url.match('#')) {
             $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
