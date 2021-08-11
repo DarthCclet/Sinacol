c5b5058249740efe30568037936f002096302ffd
Paoran <paoran_0705@hotmail.com>
2021-08-11 16:34:42 -0500

    Se quita SoftDelete a Historico Notificaciones Respuesta para correccion de

--- app/HistoricoNotificacionRespuesta.php
+++ app/HistoricoNotificacionRespuesta.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class HistoricoNotificacionRespuesta extends Model
 {
-    use SoftDeletes;
     protected $table = "historico_notificaciones_respuestas";
     protected $guarded = ['id','created_at','updated_at','deleted_at'];
     public function histrorico(){
