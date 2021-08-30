55658bf0fec15d7488bc130aafdef367c4ee53bd
Paoran <paoran_0705@hotmail.com>
2021-08-30 15:42:30 -0500

    Se cambia tipo de consulta de tipos de archivo, para consultar por plantila

--- app/Http/Controllers/DocumentoController.php
+++ app/Http/Controllers/DocumentoController.php
@@ -15,6 +15,7 @@ use App\Conciliador;
 use App\Events\GenerateDocumentResolution;
 use App\FirmaDocumento;
 use App\Parte;
+use App\PlantillaDocumento;
 use App\TipoParte;
 use App\Traits\GenerateDocument;
 use Exception;
@@ -494,9 +495,9 @@ class DocumentoController extends Controller
     public function generar_documento(){
 
         try{
-
-            $clasificacion_archivos = array_pluck(ClasificacionArchivo::whereIn('id',[13,14,15,16,17,18,40,45,41,19,43,52,54,49,56,61,62,64,66,65])->orderBy('nombre')->get(),'nombre','id');
-            return view('herramientas.regenerar_documentos', compact('clasificacion_archivos'));
+            $plantillas = array_pluck(PlantillaDocumento::orderBy('nombre_plantilla')->get(),'nombre_plantilla','id');
+            //$clasificacion_archivos = array_pluck(ClasificacionArchivo::whereIn('id',[13,14,15,16,17,18,40,45,41,19,43,52,54,49,56,61,62,64,66,65])->orderBy('nombre')->get(),'nombre','id');
+            return view('herramientas.regenerar_documentos', compact('plantillas'));
         }catch(Exception $e){
             Log::error('En script:'.$e->getFile()." En línea: ".$e->getLine().
                        " Se emitió el siguiente mensaje: ". $e->getMessage().
@@ -509,14 +510,15 @@ class DocumentoController extends Controller
 
         try{
             
-            $arrayPlantilla = [40=>6,18=>7,17=>1,16=>2,15=>3,14=>4,13=>10,19=>11,41=>8,43=>9,52=>14,54=>15,45=>12,49=>13,61=>24,56=>18,62=>19,64=>29,66=>30,65=>31];
+            //$arrayPlantilla = [40=>6,18=>7,17=>1,16=>2,15=>3,14=>4,13=>10,19=>11,41=>8,43=>9,52=>14,54=>15,45=>12,49=>13,61=>24,56=>18,62=>19,64=>29,66=>30,65=>31];
+            $arrayPlantilla = [6=>40,7=>18,17=>1,2=>16,3=>15,4=>14,10=>13,11=>19,8=>41,9=>43,14=>52,15=>54,12=>45,13=>49,24=>61,18=>56,19=>62,29=>64,30=>66,31=>65];
             $arrayPlantillaParte = [65,66,62];
             $idSolicitud = $request->get('solicitud_id',1);
             $idAudiencia = $request->get('audiencia_id');
-            $clasificacion_archivo_id = $request->get('clasificacion_archivo_id');
+            $plantilla_id = $request->get('plantilla_id');
             $idSolicitante = $request->get('solicitante_id');
             $idSolicitado = $request->get('solicitado_id');
-            $plantilla_id = $arrayPlantilla[$clasificacion_archivo_id];
+            $clasificacion_archivo_id = $arrayPlantilla[$plantilla_id];
             $id_parte_asociada = null;
             if(in_array($clasificacion_archivo_id,$arrayPlantillaParte)){
                 $id_parte_asociada = $idSolicitado;
