81a396f37e70cdbe23b128bcc4e66a10fbd3eee4
edgar-orozco <9440039+edgar-orozco@users.noreply.github.com>
2021-08-31 21:38:57 -0500

    Busqueda de nacionalidad y estado insensitivos a mayúsculas

--- app/Console/Commands/SolicitudMasiva.php
+++ app/Console/Commands/SolicitudMasiva.php
@@ -245,8 +245,8 @@ class SolicitudMasiva extends Command
 
         $abreviaturaGenero = $citado[6][0];
         $genero = Genero::where('abreviatura','ilike',$abreviaturaGenero)->first();
-        $nacionalidad = Nacionalidad::where('nombre','like','%'.$citado[7].'%')->first();
-        $estado_nacimiento = Estado::where('nombre','like','%'.$citado[8].'%')->first();
+        $nacionalidad = Nacionalidad::where('nombre','ilike','%'.$citado[7].'%')->first();
+        $estado_nacimiento = Estado::where('nombre','ilike','%'.$citado[8].'%')->first();
         $citadoObj = array(
             "id" => null,
             "nombre" => $citado[1],
