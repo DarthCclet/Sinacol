1345dc2a530b561bcbd4ef4b73162199e56c549b
edgar-orozco <9440039+edgar-orozco@users.noreply.github.com>
2021-09-24 02:41:09 -0500

    Se agrega configuración de imagen de login

--- config/logotipos.php
+++ config/logotipos.php
@@ -8,4 +8,11 @@ return [
     'logotipo-encabezado' => env('LOGOTIPO_ENCABEZADO', 'assets/img/logo/LogoEncabezado.png'),
     'logotipo-encabezado-width' => env('LOGOTIPO_ENCABEZADO_WIDTH','404'),
     'logotipo-encabezado-height' => env('LOGOTIPO_ENCABEZADO_HEIGHT','58'),
+
+    /*
+     |---------------------------------------------------------
+     | Imagen background lado izquierdo login
+     |---------------------------------------------------------
+    */
+    'imagen-background-login' => env('IMAGEN_BACKGROUND_LOGIN','assets/img/logo/fondo-verde.jpg'),
 ];
