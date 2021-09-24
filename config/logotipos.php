94b3c2729b479079c137eae0fcd0395189c4037a
edgar-orozco <9440039+edgar-orozco@users.noreply.github.com>
2021-09-24 02:24:44 -0500

    Se elimina el helper asset dado que no funciona en archivos de configuración. Genera error cuando se borra el cache de autoloader

--- config/logotipos.php
+++ config/logotipos.php
@@ -5,7 +5,7 @@ return [
      | Logotipo encabezado general
      |---------------------------------------------------------
     */
-    'logotipo-encabezado' => env('LOGOTIPO_ENCABEZADO', asset('assets/img/logo/LogoEncabezado.png')),
+    'logotipo-encabezado' => env('LOGOTIPO_ENCABEZADO', 'assets/img/logo/LogoEncabezado.png'),
     'logotipo-encabezado-width' => env('LOGOTIPO_ENCABEZADO_WIDTH','404'),
     'logotipo-encabezado-height' => env('LOGOTIPO_ENCABEZADO_HEIGHT','58'),
 ];
