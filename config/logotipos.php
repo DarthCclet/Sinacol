8240e5d4053e26715d062e251636f237cf739bf2
edgar-orozco <9440039+edgar-orozco@users.noreply.github.com>
2021-11-16 21:20:57 -0600

    Se cambia a ruta absoluta las imágenes de logotipos

--- config/logotipos.php
+++ config/logotipos.php
@@ -5,7 +5,7 @@ return [
      | Logotipo encabezado general
      |---------------------------------------------------------
     */
-    'logotipo-encabezado' => env('LOGOTIPO_ENCABEZADO', 'assets/img/logo/LogoEncabezado.png'),
+    'logotipo-encabezado' => env('LOGOTIPO_ENCABEZADO', '/assets/img/logo/LogoEncabezado.png'),
     'logotipo-encabezado-width' => env('LOGOTIPO_ENCABEZADO_WIDTH','404'),
     'logotipo-encabezado-height' => env('LOGOTIPO_ENCABEZADO_HEIGHT','58'),
 
@@ -14,7 +14,7 @@ return [
      | Imagen background lado izquierdo login
      |---------------------------------------------------------
     */
-    'imagen-background-login' => env('IMAGEN_BACKGROUND_LOGIN','assets/img/logo/fondo-verde.jpg'),
+    'imagen-background-login' => env('IMAGEN_BACKGROUND_LOGIN','/assets/img/logo/fondo-verde.jpg'),
     /*
      |---------------------------------------------------------
      | Logotipo encabezado plantillas
